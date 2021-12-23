242e4bdf31c6ac3b963b1ae640ad38db5c01d23b
Subramanian Murugan <sasubramanian_md@hotmail.com>
2021-12-23 12:25:53 +0530

    microservice patterns

---
+++ servicecommunication/hello-webclient-service/src/main/java/com/hello/client/HelloServiceFeignClient.java
@@ -0,0 +1,13 @@
+package com.hello.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//hello-service is name of the service from HelloService
+@FeignClient("hello-service")
+public interface HelloServiceFeignClient {
+    //api mapper
+    @GetMapping("/hello")
+    ResponseEntity<Hello> hello();
+}
